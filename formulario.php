2e88d06d9471c714600a82913cba13260017ee79
larissaog <oliveiragomeslarissa21@gmail.com>
2025-11-12 20:16:35 -0300

    atualizacao de style

--- formulario.php
+++ formulario.php
@@ -12,30 +12,59 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style1.css">
 </head>
-<body>
+<body class="bg-secondary-subtle">
 
-    <h1 class="title">Cadastro</h1>
+    <h1 class="title ">Cadastro</h1>
     <div class="formulario">
-         <form action="salvar.php" method="post" enctype="multipart/form-data">
+          <form action="salvar.php" method="post" enctype="multipart/form-data">
             <!-- O atributo enctype serve para avisar o navegador que o formulario vai enviar arquivos, não só textos -->
         <label for="">Nome:
-        <input type="text" name="nome" id="" required><br>
-    </label>
-
+        <input type="text" name="nome" id="input" required>
+    </label><br>
         <label for="">Email:
-            <input type="email" name="email" required><br>
-        </label>
+            <input type="email" name="email" id="input" required>
+        </label><br>
 
         <label for="">Telefone:
-            <input type="text" name="telefone" required><br>
-        </label>
+            <input type="text" name="telefone" id="input" required>
+        </label><br>
         <label for="">Foto:</label>
-        <input type="file" name=foto id=foto>
-        <button>Enviar</button>
+        <input type="file" name=foto id=foto><br><br>
+        <!-- Button trigger modal -->
+<button type="button" class="button-envair" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  enviar
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Olá. Seja bem-vindo(a)!!</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Seu cadastro foi realizado com sucesso. Aperte em fechar para continuar navegando.
+        Aceita receber informações no seu email? <br>
+        <div>
+  <input class="form-check-input" type="checkbox" id="checkboxNoLabel" value="" aria-label="...">
+</div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+      </div>
+    </div>
+  </div>
+</div>
+        <button class="button-envair" type="reset">Limpar</button>
     </form>
 
     </div>
+       
+
+   
    
 </body>
 </html>
--- includes/header.php
+++ includes/header.php
@@ -3,13 +3,67 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+  </head>
     <title>header</title>
 </head>
 <body>
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap');
+.btn{
+   background-color:#003459; 
+}
+
+.navbar-brand{
+     font-family: "Permanent Marker", cursive;
+}
+
+
+
+</style>
+
+
+
+
 
 <header>
-    <h1>Aulas-PhP</h1>
-    <hr>
+   <nav class="navbar navbar-expand-lg bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand text-light" href="#">My project with PHP</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        <li class="nav-item">
+          <a class="nav-link active text-light" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-light" href="#">Cadastro</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            utilidades
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item text-light" href="#">Action</a></li>
+            <li><a class="dropdown-item text-light" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider text-light"></li>
+            <li><a class="dropdown-item text-light" href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled text-light" aria-disabled="true">soluções</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2 text-light" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn text-light" type="submit ">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
 </header>
 <main>
     
